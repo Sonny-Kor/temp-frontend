aed44f4b2d57883edb1bd133cdbeec6e6752e6ab
SJ_Respect <46300191+Sonny-Kor@users.noreply.github.com>
2023-04-10 23:57:15 +0900

    [UPDATE] AsideBar 추가 및 Header SearchBar hover추가
    AsideBar 추가 및 Header SearchBar hover추가 하였습니다.

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import RecommendListItem from './RecommendListItem';
 
 function Header(props) {
-  return <div className="Header">
-    <div className='leftSide'>
-        <div className='frame1'>
-            <MenuIcon className='menu'/>
-        </div>
-        <div className='youTube'>
-            YOUTUBE
-        </div>
-    </div>
-    <div className='centerSide'>
-        <div className='searchBar'>
-            <div className='inputArea'></div>
-            <div className='submitButton'>
-                <SearchIcon/>
+    const [isOpenedSearchBar, setOpenSearchBar] = useState(false);
+    const toggleSearchBar = e =>{
+        setOpenSearchBar(isOpened => !isOpened);
+    }
+
+
+    return <div className="Header">
+        <div className='leftSide'>
+            <div className='frame1'>
+                <MenuIcon className='menu'/>
             </div>
-            <div className='searchBarRecommendedList'></div>
+            <Link className='youTube' to="/">
+                YOUTUBE
+            </Link>
         </div>
-    </div>
-    <div className='rightSide'>
-        <div className='loginButton'>
-            <div className='accountCircle'>
-                <AccountCircleIcon/>
-            </div>
-            <div className='login'>
-                로그인
+        <div className='centerSide'>
+            <form className='searchBar' onClick={toggleSearchBar}>
+                {isOpenedSearchBar &&
+                    <div className='searchBarRecommendedList'>
+                        <RecommendListItem/>
+                    </div>}
+                <input type="text" className='inputArea' placeholder='검색'></input>
+                <button type="submit" className='submitButton'>
+                    <SearchIcon/>
+                </button>
+            </form>
+        </div>
+        <div className='rightSide'>
+            <div className='loginButton'>
+                <div className='accountCircle'>
+                    <AccountCircleIcon/>
+                </div>
+                <div className='login'>
+                    로그인
+                </div>
             </div>
         </div>
-    </div>
-  </div>;
+    </div>;
 }
 
 
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './MainPage.scss';
 
 import VideoList from "./VideoList";
 import Header from "../../common/Header/Header"
+import AsideBar from "../../common/AssideBar/AsideBar"
 function MainPage(props) {
   return <div className="MainPage">
     <Header/>
-    <VideoList />
+    <div className='content'>
+      <AsideBar />
+      <VideoList />
+    </div>
   </div>;
 }
 
